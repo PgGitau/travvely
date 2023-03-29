d00b7491368835477f75f4fe0861a4b50cf588b9
r3t3p <92801240+PgGitau@users.noreply.github.com>
2023-03-29 21:43:50 +0300

    add scrolling to div on clicking tab button

--- resources/test.js
+++ resources/test.js
@@ -20,4 +20,38 @@ function openDestinationBy(evt, category) {
   }
   
   // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+  //document.getElementById("defaultOpen").click();
+
+
+
+  // scroll to div on tab button click
+window.smoothScroll = function (target) {
+  if (window.matchMedia("(max-width: 467px)").matches) {
+    let scrollContainer = target;
+    do {
+      //find scroll container
+      scrollContainer = scrollContainer.parentNode;
+      if (!scrollContainer) return;
+      scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    let targetY = 0;
+    do {
+      //find the top of target relatively to the container
+      if (target == scrollContainer) break;
+      targetY += target.offsetTop;
+    } while ((target = target.offsetParent));
+
+    scroll = function (c, a, b, i) {
+      i++;
+      if (i > 30) return;
+      c.scrollTop = a + ((b - a) / 30) * i;
+      setTimeout(function () {
+        scroll(c, a, b, i);
+      }, 20);
+    };
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+  }
+};
+
